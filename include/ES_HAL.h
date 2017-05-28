9c5a4a464abe9044e12cef68e2918f4bacf7acdf
mmwebster <milo.webster@gmail.com>
2017-05-28 12:37:40 -0700

    Added base HAL files

---
+++ include/ES_HAL.h
@@ -0,0 +1,20 @@
+/**
+ * @file    ES_HAL.h
+ * @brief   Main header file for the ES Framework Hardware Abstraction Layer (HAL)
+ * @details This file contains all HAL API functions. All API functions are
+ *          designed to sit on top of ES Framework s.t. a project previously no
+ *          integrated with the HAL will immediately work after integration with
+ *          HAL. To enable the HAL 
+ */
+
+
+#ifndef ES_HAL_H
+#define ES_HAL_H
+
+// System libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+#endif /* ES_HAL_H */
---
+++ templates/ES_HALConf.h
@@ -0,0 +1,11 @@
+/**
+ * @file    ES_HALConf.h
+ * @brief   Configuration file for the ES Framework Hardware Abstraction Layer (HAL)
+ * @details This configuration file allows enabling and disabling of hardware
+ *          drivers and miscelanous HAL features. The HAL is designed such
+ */
+
+#ifndef ES_HALCONF_H
+#define ES_HALCONF_H
+
+#endif /* ES_HALCONF_H */
